37466a44de265ebda2ad5c60984830b711ad0852
Vincent Wang <vwangsf@gmail.com>
2022-10-21 02:41:22 -0400

    Scale based on calculated size of campus bounding box

--- preprocess.py
+++ preprocess.py
@@ -6,7 +6,7 @@ data = json.load(open("campus.geojson"))
 for c,feature in enumerate(data["features"]):
     if "natural" in feature["properties"] and feature["properties"]["natural"] == "tree":
         coords = feature["geometry"]["coordinates"]
-        points = util.create_circle_points(coords, 5)
+        points = util.create_circle_points(coords, 3)
         print(points)
         data["features"][c]["geometry"] = {
             "type": "Polygon",
